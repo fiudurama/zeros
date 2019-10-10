205f915f71c3caf233b6c2141879042f25e1530f
fiudurama <fiudurama@gmail.com>
2019-10-10 18:58:40 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function zeros(expression) {
-  // your solution
+	let fact = expression.split('*'),
+		fiveCount = 0,
+		twoСount = 0;
+
+	fact.forEach((f) => {
+		let step = f.indexOf('!!') == -1 ? 1 : 2;
+		let num =  f.slice(0, f.indexOf('!'));
+
+		for (let i = 0; i < num; num -= step) {
+
+			let n = num;
+
+			while (true) {
+				if ( n % 5 == 0) {
+					n /= 5;
+					fiveCount++;
+				} else if (n % 2 == 0) {
+					n /= 2;
+					twoСount++;
+				} else {
+					break;
+				}
+			}
+
+		}
+	});
+
+	return Math.min(fiveCount, twoСount);
 }
